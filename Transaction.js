11b0c4cb7b4839654ddcdecbe378ec8e804a0eac
kistapuram <168419717+kistapuram@users.noreply.github.com>
2024-12-26 22:04:43 +0530

    Delete Transaction.js

--- Transaction.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose'); // Import mongoose
-const TransactionSchema = new mongoose.Schema({
-  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  amount: Number,
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Transaction', TransactionSchema);
